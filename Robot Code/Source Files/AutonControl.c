6e9f54d1c8968dcf1caefef59c6e3f8ece106df8
mindcoding <slimercraft3@gmail.com>
2018-02-25 12:21:30 -0500

    Added P control loop

--- Robot Code/Source Files/AutonControl.c
+++ Robot Code/Source Files/AutonControl.c
@@ -1,12 +1,31 @@
 const int size = 10;
 struct PControlStruct
 {
-  int SensorPorts[size];
+  int SensorPort;
   int motorPorts[size];
   int pValue[size];
+  int target;
+  bool highPrecision;
   
 };
 
 void pControlFunction(PControlStruct control)
 {
+  //int currSensorValue = SensorValue[control.SensorPort];
+  bool done = false;
+  int rangeHigh = 20;
+  int prev = 0;
+  while(SensorValue[control.SensorPort] < control.target + control.highPrecision ? control.target : 0 && !done)
+  {
+    int cumPower = 0;
+    for(int i = 0; i<size; i++)
+    {
+      cumPower += control.pValue[i] * (SensorValue[control.SensorPort];
+    }
+    for(int i = 0; i<size; i++)
+      motor[control.motorPorts[i]] = cumPower; 
+    if(abs(prev - SensorValue[control.SensorPort]) < rangeHigh && abs(SensorValue[control.SensorPort] - control.target) < rangeHigh)
+      done = true;
+    prev = SensorValue[control.SensorPort];
+  }
 }
