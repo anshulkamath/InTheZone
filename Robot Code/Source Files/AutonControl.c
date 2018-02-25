33e261366eef92e3dee07728651eddd221ffee92
mindcoding <slimercraft3@gmail.com>
2018-02-25 11:25:09 -0500

    Update AutonControl.c

--- Robot Code/Source Files/AutonControl.c
+++ Robot Code/Source Files/AutonControl.c
@@ -1,1 +1,12 @@
+const int size = 10;
+struct PControlStruct
+{
+  int SensorPorts[size];
+  int motorPorts[size];
+  int pValue[size];
+  
+};
 
+void pControlFunction(PControlStruct control)
+{
+}
