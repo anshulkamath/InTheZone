d0e200266148371b77cd41769cb738948a560334
mindcoding <slimercraft3@gmail.com>
2018-03-29 20:55:14 -0400

    03_29_2018

--- Github Scripts/gitUpload.bat
+++ Github Scripts/gitUpload.bat
@@ -5,7 +5,7 @@ for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
      set year=%%l
 )
 set datestr=%month%_%day%_%year%
-cd C:\Users\Robot\Z Code\InTheZone
+cd C:\Users\Robot\Z-Code\InTheZone
 git add -A
 git commit -m %datestr%
 git push https://github.com/anshulkamath/InTheZone master
